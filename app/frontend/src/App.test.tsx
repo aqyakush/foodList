64808ba70d1669f66a2b225eac5e5a4d46120ac2
Anton Yakushonok <anton.yakushonok@gmail.com>
2023-09-01 18:18:27 +0300

    frontend: Migrate to typescript

--- app/frontend/src/App.test.tsx
+++ app/frontend/src/App.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders hello', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
