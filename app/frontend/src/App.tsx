e6a4155bbae995922c2a171ef76c9d0b8fbfca22
Anton Yakushonok <anton.yakushonok@gmail.com>
2023-12-04 19:42:50 +0200

    frontend: Create navigation for recieps and meal plan

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import SearchRecipe from './pages/mainPage';
 import Test from './pages/test';
+import NavBar from './components/Navigation';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Routes>
         <Route path="/" element={<SearchRecipe />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/mealPlans" element={<div>Meal Plans</div>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ app/frontend/src/components/Navigation/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNavLink = styled(NavLink)`
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #111;
+  }
+
+  &.active {
+    background-color: #4CAF50;
+  }
+`;
+
+const StyledNav = styled.nav`
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+const NavBar = () => {
+    return (
+        <StyledNav>
+            <ul>
+                <li>
+                    <StyledNavLink to="/">Recipes</StyledNavLink>
+                </li>
+                <li>
+                    <StyledNavLink to="/mealPlans">Meal Plans</StyledNavLink>
+                </li>
+            </ul>
+        </StyledNav>
+    );
+}
+
+export default NavBar;
