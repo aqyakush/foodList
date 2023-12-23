d7aa269a13a289c4196ba930aab84b15f7d3828d
Anton Yakushonok <anton.yakushonok@gmail.com>
2023-12-23 12:55:19 +0200

    Update github action to wait for ingress

---
+++ utils/wait_for_ingress.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INGRESS_NAME=foodlist-ingress
+while true; do
+  ADDRESS=$(kubectl get ingress $INGRESS_NAME -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  if [ -z "$ADDRESS" ]; then
+    echo "Waiting for Ingress to get an address..."
+    sleep 10
+  else
+    echo "Ingress has address: $ADDRESS"
+    break
+  fi
+done
