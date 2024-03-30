8ee9d8d5b56afa7bdf012efb61973732167c57e0
Anton Yakushonok <anton.yakushonok@gmail.com>
2024-03-30 08:20:11 +0200

    Another fix for rebuild script

--- utils/rebuild.sh
+++ utils/rebuild.sh
@@ -7,20 +7,15 @@ cd  ~/foodList
 git checkout main
 git pull origin main
 
-# Check if there are running containers
-if [ "$(docker ps -q)" != "" ]; then
-    # Stop all running containers
-    sudo docker stop $(docker ps -q)
-fi
-
-# Remove all stopped containers
-sudo docker rm $(docker ps -a -q)
+sudo docker rm -f $(docker ps -a -q)
 
 # Check if there are any Docker images
 if [ "$(docker images -q)" != "" ]; then
     # Remove all Docker images
-    sudo docker rmi $(docker images -q)
+    sudo docker rmi -f $(docker images -q)
 fi
+
+docker image prune -f
 
 cd  ~/foodList/app
 
