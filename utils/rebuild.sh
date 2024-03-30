774875bf0498dcd9260512a8a50c3b3730a5ebb2
Anton Yakushonok <anton.yakushonok@gmail.com>
2024-03-30 08:22:56 +0200

    Another one and another one and another one

--- utils/rebuild.sh
+++ utils/rebuild.sh
@@ -10,18 +10,17 @@ git pull origin main
 # Check if there are running containers
 if [ "$(docker ps -q)" != "" ]; then
     # Stop all running containers
-    sudo docker rm -f $(docker ps -a -q)
+    docker rm -f $(docker ps -a -q)
 fi
 
 # Check if there are any Docker images
 if [ "$(docker images -q)" != "" ]; then
     # Remove all Docker images
-    sudo docker rmi $(docker images -q)
+    docker rmi $(docker images -q)
 fi
 
 docker image prune -f
 
 cd  ~/foodList/app
 
-# Run docker-compose up
-docker-compose up -d
+docker compose up -d
